8377da314890b1a903113b7f2557217697e36e88
ehroy <68073896+ehroy@users.noreply.github.com>
2022-02-10 14:14:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,30 +51,7 @@ const IP = () =>
         reject(err);
       });
   });
-const cek = (domain, email) =>
-  new Promise((resolve, reject) => {
-    fetch(`https://generator.email/inbox1`, {
-      method: "get",
-      headers: {
-        accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3",
-        "accept-encoding": "gzip, deflate, br",
-        cookie: `_ga=GA1.2.659238676.1567004853; _gid=GA1.2.273162863.1569757277; embx=%5B%22${email}%40${domain}%22%2C%22hcycl%40nongzaa.tk%22%5D; _gat=1; io=io=tIcarRGNgwqgtn40O${randstr(
-          3
-        )}; surl=${domain}%2F${email}`,
-        "upgrade-insecure-requests": 1,
-        "user-agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
-      },
-    })
-      .then((res) => res.text())
-      .then((res) => {
-        resolve(res);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+
 const generateIndoName = () =>
   new Promise((resolve, reject) => {
     fetch("https://swappery.site/data.php?qty=1", {
@@ -88,7 +65,7 @@ const generateIndoName = () =>
         reject(err);
       });
   });
-const getRegist = (email, nama, IPcon,reff) =>
+const getRegist = (email, nama, IPcon, reff) =>
   new Promise((resolve, reject) => {
     fetch("https://id.vscore.vn/api-v1/accounts/register/4", {
       method: "POST",
@@ -157,46 +134,48 @@ const Otp = (token, otpa, IPcon) =>
   });
 (async () => {
   while (true) {
-    try {
-      const reff = "2II9LVLV"
-      const domain = "smilevxer.com";
-      const indoName = await generateIndoName();
-      const rand = await randstr(4);
-      const { result } = indoName;
-      const name =
-        result[0].firstname.toLowerCase() +
-        result[0].lastname.toLowerCase() +
-        rand;
-      const email = `${name}@${domain}`;
-      console.log("[+] Mendaftar dengan email " + email);
-      const IPcon = await IP();
-      console.log("[=] IP " + IPcon);
-      const Prosesregist = await getRegist(email, name, IPcon, reff);
-      if (Prosesregist.success === true) {
-        const tokenreg = Prosesregist.data.token;
-        // let linkConfirm;
-        let count = 0;
-        do {
-          linkConfirm = await functionGetLink(
-            email,
-            email.split("@")[0],
-            email.split("@")[1]
-          );
-          count ++;
-          // console.log(count)
-        } while (count < 10);
-        const otp = linkConfirm;
-        const validateOtp = await Otp(tokenreg, otp, IPcon);
-        if (validateOtp.success === true) {
-          console.log("[+] Berhasil mendaftar");
-        } else {
-          console.log(validateOtp.message);
-        }
+    const reff = "2II9LVLV";
+    const domain = "ehroyhehe.my.id";
+    const indoName = await generateIndoName();
+    const rand = await randstr(4);
+    const { result } = indoName;
+    const name =
+      result[0].firstname.toLowerCase() +
+      result[0].lastname.toLowerCase() +
+      rand;
+    const email = `${name}@${domain}`;
+    console.log("[+] Mendaftar dengan email " + email);
+    const IPcon = await IP();
+    console.log("[=] IP " + IPcon);
+    const Prosesregist = await getRegist(email, name, IPcon, reff);
+    console.log("[+] " + Prosesregist.message);
+    if (Prosesregist.success === true) {
+      const tokenreg = Prosesregist.data.token;
+      // let linkConfirm;
+      console.log("[+] Menunggu 10 detik");
+      let count = 0;
+      do {
+        linkConfirm = await functionGetLink(
+          email,
+          email.split("@")[0],
+          email.split("@")[1]
+        );
+        count++;
+        // console.log(count);
+      } while (count < 10);
+      const otp = linkConfirm;
+      console.log("[+] Menemukan Kode OTP => " + otp);
+      const validateOtp = await Otp(tokenreg, otp, IPcon);
+      // console.log(validateOtp);
+      if (validateOtp.success === true) {
+        console.log("[+] Berhasil mendaftar");
       } else {
-        console.log("[=] Ada kesalahan ");
+        console.log(validateOtp.message);
       }
-    } catch (error) {
-      console.log("[+] Timeout");
+    } else {
+      console.log(
+        "[!] Silakan Ganti Domain anda Sendir jika Domain Bawaan Tidak Bisa di pake "
+      );
     }
   }
 })();
